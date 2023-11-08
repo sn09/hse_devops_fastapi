27aea692ea8d50e90ca68a75ab466744417a9ce4
Nikita <saraevnik0909@gmail.com>
2023-11-09 00:20:01 +0300

    modify main.py

--- main.py
+++ main.py
@@ -1,12 +1,26 @@
+import datetime
+import uuid
 from enum import Enum
+from typing import List
 
-from fastapi import FastAPI
-from pydantic import BaseModel
+from fastapi import FastAPI, Request
+from fastapi.responses import JSONResponse
+from pydantic import BaseModel, Field
 
 app = FastAPI()
 
 
+def _generate_int_id() -> int:
+    return uuid.uuid4().int
+
+
+class InvalidKeyException(Exception):
+    def __init__(self, msg: str):
+        self.msg = msg
+
+
 class DogType(str, Enum):
+    """An enumeration."""
     terrier = "terrier"
     bulldog = "bulldog"
     dalmatian = "dalmatian"
@@ -14,7 +28,7 @@ class DogType(str, Enum):
 
 class Dog(BaseModel):
     name: str
-    pk: int
+    pk: int = Field(default_factory=_generate_int_id) # By documentaion it's not required ???
     kind: DogType
 
 
@@ -39,10 +53,55 @@ post_db = [
 ]
 
 
-@app.get('/')
-def root():
-    # ваш код здесь
-    ...
+@app.exception_handler(InvalidKeyException)
+async def invalid_key_handler(request: Request, exc: InvalidKeyException) -> JSONResponse:
+    return JSONResponse(
+        status_code=418,
+        content={"message": exc.msg},
+    )
 
-# ваш код здесь
-...
+
+@app.get("/")
+async def root() -> JSONResponse:
+    return JSONResponse(content="Hello")
+
+
+@app.post("/post")
+def get_post() -> Timestamp:
+    obj_id = _generate_int_id()
+    obj_ts = int(datetime.datetime.now().timestamp())
+    new_ts = Timestamp(id=obj_id, timestamp=obj_ts)
+    post_db.append(new_ts)
+    return new_ts
+
+
+@app.get("/dog")
+def get_dogs(kind: DogType = None) -> List[Dog]:
+    current_dogs = list(dogs_db.values())
+    if kind:
+        current_dogs = list(filter(lambda x: x.kind == kind, current_dogs))
+    return current_dogs
+
+
+@app.post("/dog")
+def create_dog(dog: Dog) -> Dog:
+    if dog.pk in dogs_db:
+        raise InvalidKeyException(msg=f"Key {dog.pk} already exists")
+    dogs_db[dog.pk] = dog
+    return dog
+
+
+@app.get("/dog/{pk}")
+def get_dog_by_pk(pk: int) -> Dog:
+    dog = dogs_db.get(pk)
+    if not dog:
+        raise InvalidKeyException(msg=f"No dog found for key {pk}")
+    return dog
+
+
+@app.patch("/dog/{pk}")
+def update_dog(pk: int, dog: Dog) -> Dog:
+    if pk != dog.pk:
+        raise InvalidKeyException(msg=f"Passed pk is {pk}, but dog pk is {dog.pk}")
+    dogs_db[pk] = dog
+    return dog
